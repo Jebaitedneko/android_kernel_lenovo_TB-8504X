33268e7ab8409f0f206ef3815e79833e6318024b
Jebaitedneko <Jebaitedneko@gmail.com>
2021-09-25 04:39:00 +0530

    drivers/platform/msm: import
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- drivers/platform/msm/qpnp-power-on.c
+++ drivers/platform/msm/qpnp-power-on.c
@@ -509,6 +509,55 @@ qpnp_get_cfg(struct qpnp_pon *pon, u32 pon_type)
 
 static DEVICE_ATTR(debounce_us, 0664, qpnp_pon_dbc_show, qpnp_pon_dbc_store);
 
+//--add litao1 for ota mask long press reboot-start
+static ssize_t qpnp_kpdpwr_reset_show(struct device * dev,
+  struct device_attribute * attr, char * buf) {
+  struct qpnp_pon * pon = dev_get_drvdata(dev);
+  u8 val;
+  int rc;
+
+  rc = spmi_ext_register_readl(pon -> spmi -> ctrl, pon -> spmi -> sid, QPNP_PON_S3_SRC(pon), & val, 1);
+  if (rc) {
+    pr_err("Unable to read pon_dbc_ctl rc=%d\n", rc);
+    return rc;
+  }
+
+  rc = spmi_ext_register_readl(pon -> spmi -> ctrl, pon -> spmi -> sid, QPNP_PON_KPDPWR_S2_CNTL2(pon), & val, 1);
+  if (rc) {
+    pr_err("Unable to read pon_dbc_ctl rc=%d\n", rc);
+    return rc;
+  }
+  val &= QPNP_PON_S2_RESET_ENABLE;
+  val = val >> 7;
+
+  return snprintf(buf, QPNP_PON_BUFFER_SIZE, "%d\n", val);
+}
+
+static ssize_t qpnp_kpdpwr_reset_store(struct device * dev,
+  struct device_attribute * attr,
+  const char * buf, size_t size) {
+  struct qpnp_pon * pon = dev_get_drvdata(dev);
+  u32 value;
+  int rc;
+
+  if (size > QPNP_PON_BUFFER_SIZE)
+    return -EINVAL;
+
+  rc = kstrtou32(buf, 10, & value);
+  if (rc)
+    return rc;
+
+  value = value << 7;
+  //value &= QPNP_PON_S2_RESET_ENABLE;
+
+  rc = qpnp_pon_masked_write(pon, QPNP_PON_KPDPWR_S2_CNTL2(pon), QPNP_PON_S2_RESET_ENABLE, value);
+
+  return size;
+}
+
+static DEVICE_ATTR(kpdpwr_reset, 0664, qpnp_kpdpwr_reset_show, qpnp_kpdpwr_reset_store);
+////--add litao1 for ota mask long press reboot--end
+
 #define PON_TWM_ENTRY_PBS_BIT		BIT(0)
 static int qpnp_pon_reset_config(struct qpnp_pon *pon,
 		enum pon_power_off_type type)
@@ -2399,6 +2448,19 @@ static int qpnp_pon_probe(struct spmi_device *spmi)
 			rc);
 		return rc;
 	}
+
+
+	////--add litao1 for ota mask long press reboot-start
+
+	/*add for OTA mask powerkey*/
+	rc = device_create_file(&spmi->dev, &dev_attr_kpdpwr_reset);
+		if (rc) {
+			dev_err(&spmi->dev, "sys file creation failed rc: %d\n", rc);
+			return rc;
+		}
+
+	////--add litao1 for ota mask long press reboot--end
+
 
 	if (of_property_read_bool(spmi->dev.of_node,
 					"qcom,pon-reset-off")) {
